06bbdabb307e5979b18384c11c780ec9e8e61143
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-04-18 17:03:40 +0100

    fix: set default port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const todoRoutes = require("./routes/todos");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
