518006f3c93844d4ae1bf96065f9d7257bd716fa
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-04-25 14:23:36 +0100

    feat: add authentication middleware to protect todo routes

--- routes/todos.js
+++ routes/todos.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { getTodos, createTodo, deleteTodo, toggleTodo } = require("../controllers/todoControllers");
+const protect = require("../middleware/authMiddleware");
 
 // Define routes
-router.get("/", getTodos);         // GET
-router.post("/", createTodo);        // POST
+router.get("/", protect, getTodos);         // GET
+router.post("/", protect, createTodo);        // POST
 router.delete("/:_id", deleteTodo); // DELETE
 router.patch("/:_id", toggleTodo); // PATCH 
 module.exports = router;
